1e42b24c665b28b4046dd509e703197c50e12e61
Jake <79664479+PirateJake2000@users.noreply.github.com>
2024-08-04 23:58:10 +0100

    miner update

---
+++ StripMiner.lua
@@ -0,0 +1,75 @@
+-- Imports
+vector = require("Library.Vector")
+ins = require("Library.ins")
+
+-- Setup
+local slave = ins(vector(-261,78,-70), vector(0,0,-1))
+
+-- Main
+-- Turtle --
+-- dig forward
+-- move forward
+-- check up
+-- check down
+-- turn left
+-- check forward
+-- turn right
+-- turn right
+-- check forward
+-- turn left
+
+-- if any check is ore then mineVein()
+-- then return to tunnel
+
+
+-- Functions
+function mineVein()
+
+end
+
+-- Settings
+local startPos = slave.position
+local startFacing = slave.facing
+local mineDepth = 50
+
+local rows = 8
+local rowLength = 32
+
+-- go down to mineDepth
+while slave.position.z > mineDepth do
+    turtle.digDown()
+    slave:down()
+end
+
+
+local currentRow = 1
+--------------------
+:: mine ::
+--------------------
+for i=1,rowLength do
+    turtle.dig()
+    slave:forward()
+
+    -- Check around for ores
+end
+
+if slave.facing == startFacing then
+    slave:turnRight()
+    turtle.dig()
+    slave:forward()
+    turtle.dig()
+    slave:forward()
+    slave:turnRight()
+else
+    slave:turnLeft()
+    turtle.dig()
+    slave:forward()
+    turtle.dig()
+    slave:forward()
+    slave:turnLeft()
+end
+
+currentRow = currentRow + 1
+--------------------
+if (currentRow < rows) then goto mine else return end
+--------------------
