8e3f309e3089f45d7ba71fed1a9235c65b401a31
Jake <79664479+PirateJake2000@users.noreply.github.com>
2024-08-04 23:15:02 +0100

    eee

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
 -- Imports
-vector = require("vector")
-ins = require("ins")
+vector = require("Library.Vector")
+ins = require("Library.ins")
 
 -- Setup
 local slave = ins(vector(-2806, 64, 3048), vector(0,0,-1))
