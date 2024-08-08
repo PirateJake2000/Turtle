19820f3d53468ab803b4956c9edc844b6fe919f3
Jake <79664479+PirateJake2000@users.noreply.github.com>
2024-08-08 21:22:45 +0100

    Refactor turtle setup and fuel management in StripMineMaster.lua

--- StripMineMaster.lua
+++ StripMineMaster.lua
@@ -9,23 +9,75 @@ ins = require("Library.ins")
 -- 2 -> Lowest turtle 
 -- 3 -> Rows 
 -- 4 -> RowLength
+local turtleCount = args[1]
+local mineDepth = args[2] -- Lowest Turtle
+local rows = 16
+local rowLength = 16 * 3
+local extraFuel = 64
+local turtlesSent = 0
 
 -- Setup
-local slave = ins(vector(-444,90,-331), vector(-1,0,0))
+local slave = ins(vector(-2928,67,3119), vector(-1,0,0))
 
--- Main
-function spawnAndSetupTurtle()
-    -- Find turtle in inventory
-    for i=1,16 do 
-        if turtle.getItemCount(i) > 0 and turtle.getItemDetail(i).name == "computercraft:turtle_normal" then
-            turtle.select(i)
-            turtle.place()
-            return
+
+-- select item in inventory
+function selectItem(itemName)
+    for i=1,16 do
+        turtle.select(i)
+        local _,detail = turtle.getItemDetail
+        if detail.name == itemName then return true end
+    end
+
+    return false
+end
+
+
+
+-- place turtle below and send it to its altitude
+function sendTurtle()
+    if not selectItem("computercraft:turtle_normal") then return end
+
+    turtle.placeDown()
+    
+    selectItem("minecraft:coal")
+    
+    -- amount of coal needed
+    local altDifference = math.abs(slave:position.y - (mineDepth - turtlesSent*5))
+    local coal = ((rows * (rowLength + 3) + altDifference * 2 ) + extraFuel) / 80
+    
+    -- tell turtle how deep to go by amount of red wool
+    selectItem("minecraft:red_wool") -- not sure about name
+    turtle.dropDown(math.floor((mineDepth - turtlesSent*5)/5))
+
+
+end
+
+
+-- Check for chest
+function setUp()
+
+    local function checkForChest()
+        for i = 1, 16 do
+            if turtle.getItemDetail(i) ~= nil then
+                if turtle.getItemDetail(i).name == "minecraft:chest" then
+                    -- Place chest above turtle
+                    turtle.select(i)
+                    turtle.placeUp()
+                end
+            end
         end
     end
 
-    -- Run update script on the placed turtle
-    peripheral.call("front", "run", "update")
+    checkForChest()
+    -- Check to see if a chest was placed
+    if turtle.inspectUp().name ~= "minecraft:chest" then
+        os.sleep(3)
+        checkForChest()
+    end
+
+    turtle.digDown()
+    
+    
 end
 
-a=peripheral.getMethods("front") for i,v in pairs(a) do print(v) end
+setUp()
